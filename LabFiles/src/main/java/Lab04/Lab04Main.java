f945ec6fb561658fa1d139fa8d2721a8861480ef
iamnanthakishor <nanthunan255@gmail.com>
2026-05-15 17:22:32 +0530

    Update StaffCount in Main

--- LabFiles/src/main/java/Lab04/Lab04Main.java
+++ LabFiles/src/main/java/Lab04/Lab04Main.java
@@ -2,9 +2,16 @@
 package Lab04;
 public class Lab04Main {
     public static void main (String args[]){
+        
+        StaffMember.showSystemName();
+        
         Lecturer L1 = new Lecturer("Alice Smith", "L001", "Computing", 3, 80000.0);
         Lecturer L2 = new Lecturer("Bob Jones", "L002", "Engineering", 2, 100000.0);
         LabAssistant Lab1 = new LabAssistant("Charlie Brown", "A001", "Science", 30, 1500.0);
+        
+        System.out.println("Total Number of Staffs: " + StaffMember.getStaffCount());
+        System.out.println("\n");
+        
         L1.displayLecturerDetails();
         System.out.println("Monthly Payment: " + L1.calculateMonthlyPayment());
         L2.displayLecturerDetails();
@@ -13,6 +20,12 @@ public class Lab04Main {
         System.out.println("Monthly Payment: " + Lab1.calculateMonthlyPayment());
         
         UniversityPolicy.showPolicyHeader();
+        System.out.println("University: " + UniversityPolicy.UNIVERSITY_NAME);
+        
+        System.out.println("\nBonus:");
+        System.out.println("Lecturer 1 Bonus: " + UniversityPolicy.calculateBonus(L1.calculateMonthlyPayment()));
+        System.out.println("Lecturer 1 Bonus: " + UniversityPolicy.calculateBonus(L2.calculateMonthlyPayment()));
+        System.out.println("Lab Assistent Bonus: " + UniversityPolicy.calculateBonus(Lab1.calculateMonthlyPayment()));
     }
 }
 // Comment: department can be used here because it is protected in the parent class
