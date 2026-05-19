1f840b1393264fa2b821573631f416c93c59ff7a
iamnanthakishor <nanthunan255@gmail.com>
2026-05-19 21:37:24 +0530

    Update StudentGradeForm.java

--- LabQuestions/src/main/java/Lab06/StudentGradeForm.java
+++ LabQuestions/src/main/java/Lab06/StudentGradeForm.java
@@ -20,6 +20,7 @@ public class StudentGradeForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -29,6 +30,17 @@ public class StudentGradeForm extends javax.swing.JFrame {
         outputTextArea = new javax.swing.JTextArea();
         nameTextField = new javax.swing.JTextField();
         markTextField = new javax.swing.JTextField();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -155,6 +167,7 @@ public class StudentGradeForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField markTextField;
     private javax.swing.JTextField nameTextField;
